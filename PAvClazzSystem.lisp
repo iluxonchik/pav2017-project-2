be0d5bc93f8aa6de6ff2e128cbb241cc82e48a95
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2017-05-18 15:00:15 +0100

    add necessary 'getters'

--- PAvClazzSystem.lisp
+++ PAvClazzSystem.lisp
@@ -1,3 +1,4 @@
+;; TABLE TO HOLD ALL META CLASS OBJECTS
 (let ((all-classes (make-hash-table)))
 
     (defun get-class (name)
@@ -11,6 +12,40 @@
                     (error "Class ~S does not exist" name)
                     class-object)))
 )
+;; META CLASS DEFINITION
+
+;;; GETTERS
+(defun class-direct-slots (class)
+    (let* ((class-object (get-class-with-error class))
+            (direct-slots (gethash 'direct-slots class-object)))
+        direct-slots))
+
+(defun class-all-slots (class)
+    (let* ((class-object (get-class-with-error class))
+            (all-slots (gethash 'all-slots class-object)))
+        all-slots))
+
+(defun class-direct-superclasses (class)
+    (let* ((class-object (get-class-with-error class))
+            (direct-superclasses (gethash 'direct-superclasses class-object)))
+        direct-superclasses))
+
+(defun class-direct-subclasses (class)
+    (let* ((class-object (get-class-with-error class))
+            (direct-subclasses (gethash 'direct-subclasses class-object)))
+        direct-subclasses))
+
+(defun class-cpl (class)
+    (let* ((class-object (get-class-with-error class))
+            (cpl (gethash 'cpl class-object)))
+        cpl))
+
+;;; FINISH GETTERS
+;unsure of how to implement interssesion at the
+;moment so its better to leave this blank
+;;; SETTERS
+
+;;; FINISH SETTERS
 
 (defun make-class-def (name &key direct-slots all-slots direct-superclasses direct-subclasses cpl)
     (if (get-class name)
@@ -24,15 +59,9 @@
          (setf (gethash 'cpl class) cpl)
          (setf (get-class name) class))))
 
-(defun class-direct-slots (class)
-    (let* ((class-object (get-class-with-error class))
-            (direct-slots (gethash 'direct-slots class-object)))
-        direct-slots))
+;; FINISH META CLASS DEFINITION
 
-(defun class-all-slots (class)
-    (let* ((class-object (get-class-with-error class))
-            (all-slots (gethash 'all-slots class-object)))
-        all-slots))
+;; DEFMACRO DEF-CLASS HELPER FUNCTIONS
 
 (defun canonize-class-slots (direct-slots class-precedance-list)
     direct-slots)
@@ -56,22 +85,27 @@
     `(defun (setf ,(!suffix-for-def-class class-name slot-name)) (new-value object)
         (setf (gethash ',slot-name object) new-value)))
 
+;; FINISH DEFMACRO DEF-CLASS HELPER FUNCTIONS
+
 (defmacro def-class (class &rest slots)
       (let* ((class-definition (if (listp class) class (list class nil)))
         (name (first class-definition))
         (super-classes (cdr class-definition)))
-        `(progn (make-class-def ',name 
+        `(progn (make-class-def ',name
                     :direct-superclasses ',(canonize-direct-superclasses super-classes)
                     :direct-slots ',slots
                     :all-slots ',(canonize-class-slots slots super-classes))
                 (defun ,(!suffix-for-def-class 'make name) (&key ,@slots)
                           (let ((class (make-hash-table)))
+                            ()
                             ,@(mapcar #'(lambda (x) `(setf (gethash ',x class) ,x)) slots)
                           class))
                 ,@(mapcar #'(lambda (x) (!create-getter-for-class name x)) slots)
                 ,@(mapcar #'(lambda (x) (!create-setter-for-class name x)) slots)
                 )
         ) )
+
+;;;FINISH PAva CLASS SYSTEM
 
 ;;Some tests
 (def-class person name)
