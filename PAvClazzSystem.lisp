240cf6a8cd5d08178478b7fd499ce01e211a6c75
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2017-05-18 20:15:42 +0100

    passes all tests

--- PAvClazzSystem.lisp
+++ PAvClazzSystem.lisp
@@ -71,55 +71,23 @@
 ;; (MACRO DEF-CLASS) HELPER FUNCTIONS
 
 (defun canonize-class-slots (direct-slots class-precedance-list)
-    direct-slots)
+    (flet ((illegal-slot-name (slot reason)
+                              (error "The slot ~a cannot be defined. The slot ~a" slot reason)))
+          (let ((all-slots (append direct-slots (apply #'append (loop for class in (cdr class-precedance-list)
+                                                                 collect (class-direct-slots class))))))
+            (loop for slot in all-slots
+                do (cond ((not (symbolp slot))(illegal-slot-name slot "is not a symbol."))
+                      ((keywordp slot)(illegal-slot-name slot "is a keyword."))
+                      ;((member name **reserved-slots**)(illegal-slot-name slot "is a reserved keyword."))
+                      ;;((member slot all-slots)(illegal-slot-name slot "is already defined somewhere else."))
+                  ))
+            all-slots)))
 
 (defun canonize-direct-superclasses (super-classes)
     (if (null (first super-classes))
         super-classes
         (progn (mapcar #'(lambda (class-symbol) (get-class-with-error class-symbol)) super-classes)
             super-classes)))
-
-(defun class-precedance-list (class direct-superclasses)
-    ;empty method right now waiting for ILLYA
-    direct-superclasses)
-
-(defun !suffix-for-def-class (symbol suffix)
-  (intern (concatenate 'string (string symbol) "-" (string suffix))))
-
-(defun !create-getter-for-class (class-name slot-name)
-    `(defun ,(!suffix-for-def-class class-name slot-name) (object)
-        (let ((value (gethash ',slot-name object)))
-            ;need to check is object is of the class which this getter is made for
-            value)))
-
-(defun !create-setter-for-class (class-name slot-name)
-    `(defun (setf ,(!suffix-for-def-class class-name slot-name)) (new-value object)
-        (setf (gethash ',slot-name object) new-value)))
-
-;; FINISH (MACRO DEF-CLASS) HELPER FUNCTIONS
-
-(defmacro def-class (class &rest slots)
-      (let* ((class-definition (if (listp class) class (list class 'standard-pav-class)))
-        (name (first class-definition))
-        (super-classes (cdr class-definition)))
-        `(progn (make-class-def ',name
-                    :direct-superclasses ',(canonize-direct-superclasses super-classes)
-                    :direct-slots ',slots
-                    :all-slots ',(canonize-class-slots slots super-classes)
-                    :cpl ',(class-precedance-list name super-classes))
-                ;(mapcar #'(lambda (x) (add-direct-subclass x ',name)) ',super-classes)
-                (dolist (class ',super-classes) (add-direct-subclass class  ',name))
-                (defun ,(!suffix-for-def-class 'make name) (&key ,@slots)
-                          (let ((class (make-hash-table)))
-                            ,@(mapcar #'(lambda (x) `(setf (gethash ',x class) ,x)) slots)
-                          class))
-                ,@(mapcar #'(lambda (x) (!create-getter-for-class name x)) slots)
-                ,@(mapcar #'(lambda (x) (!create-setter-for-class name x)) slots)
-                )
-        ) )
-
-
-;;;FINISH PAva CLASS SYSTEM
 
 (defun list-empty (lst)
     (eql (length lst) 0)
@@ -144,30 +112,67 @@
                  )
          )
     (setf cpl_res (remove-duplicates cpl_res))
-    (print cpl_res)
      cpl_res
      )
 )
 
-(defun cpl (cls ds)
-  (class-precedance-list cls ds))
+(defun !suffix-for-def-class (list-of-symbols)
+  (intern (format nil "~{~a~}" list-of-symbols)))
+
+(defun !create-getter-for-class (class-name slot-name)
+    `(defun ,(!suffix-for-def-class (list class-name '- slot-name)) (object)
+        (let ((value (gethash ',slot-name object)))
+            ;need to check is object is of the class which this getter is made for
+            value)))
+
+(defun !create-setter-for-class (class-name slot-name)
+    `(defun (setf ,(!suffix-for-def-class (list class-name '- slot-name))) (new-value object)
+        (setf (gethash ',slot-name object) new-value)))
+
+;; FINISH (MACRO DEF-CLASS) HELPER FUNCTIONS
+
+(defmacro def-class (class &rest slots)
+      (let* ((class-definition (if (listp class) class (list class 'standard-pav-class)))
+        (name (first class-definition))
+        (super-classes (cdr class-definition))
+        (cpl (class-precedance-list name super-classes))
+        (all-slots (canonize-class-slots slots cpl)))
+        `(progn (make-class-def ',name
+                    :direct-superclasses ',(canonize-direct-superclasses super-classes)
+                    :direct-slots ',slots
+                    :all-slots ',all-slots
+                    :cpl ',cpl)
+                (mapc #'(lambda (x) (add-direct-subclass x ',name)) ',super-classes)
+                (defun ,(!suffix-for-def-class (list 'make '- name)) (&key ,@all-slots)
+                          (let ((class (make-hash-table)))
+                            (setf (gethash 'meta-object-name class) ',name)
+                            ,@(mapcar #'(lambda (x) `(setf (gethash ',x class) ,x)) all-slots)
+                          class))
+                (defun ,(!suffix-for-def-class (list name '?)) (object)
+                    (let ((meta-object (gethash 'meta-object-name object)))
+                  (not (null (member ',name (class-cpl meta-object))))))
+                ,@(mapcar #'(lambda (x) (!create-getter-for-class name x)) all-slots)
+                ,@(mapcar #'(lambda (x) (!create-setter-for-class name x)) all-slots)
+                )
+        ) )
+
+
+;;;FINISH PAva CLASS SYSTEM
 
 
 ;;Some tests
-(def-class person name)
-(macroexpand '(def-class (student person) name hello))
-
-(def-class (student person) id)
-
-(defvar a)
-(setf a (make-student :id "ist177900"))
-(student-id a)
-(setf (student-id a) "ist177966")
-(student-id a)
-
-(def-class h)
-(def-class f)
-(def-class (a h f))
-(def-class (b a))
-(def-class (c a))
-(def-class (d b c))
+; (def-class person name)
+; (def-class (student person) id)
+;
+; (defvar a)
+; (setf a (make-student :id "ist177900"))
+; (student-id a)
+; (setf (student-id a) "ist177966")
+; (student-id a)
+;
+; (def-class h)
+; (def-class f)
+; (def-class (a h f))
+; (def-class (b a))
+; (def-class (c a))
+; (def-class (d b c))
